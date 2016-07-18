0d49a39e2af59c7073e19b191654333cd4322951
lidaoshanying <lidaoshanying@163.com>
2016-07-18 16:20:55 +0800

    master文件更新

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -81,19 +81,10 @@
 
 </div>
 
-<div id="style-switcher">
-    <i class="icon-arrow-left icon-white"></i>
-    <span>Style:</span>
-    <a href="#grey" style="background-color: #555555;border-color: #aaaaaa;"></a>
-    <a href="#blue" style="background-color: #2D2F57;"></a>
-    <a href="#red" style="background-color: #673232;"></a>
-</div>
+
 
 <div id="content">
     @yield('content')
-
-
-
 </div>
 <div class="row-fluid">
     <div id="footer" class="span12">
